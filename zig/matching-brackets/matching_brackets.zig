59a06d1c90814bb3a50aff2e7a355b7a47853796
Alexander Alekhin <sphrand@gmail.com>
2024-05-21 08:22:48 +0300

    Updating solution

--- zig/matching-brackets/matching_brackets.zig
+++ zig/matching-brackets/matching_brackets.zig
@@ -2,7 +2,16 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
-    _ = allocator;
-    _ = s;
-    @compileError("please implement the isBalanced function");
+    var stack = std.ArrayList(u8).init(allocator);
+    defer stack.deinit();
+    for (s) |c| {
+        switch (c) {
+            '(', '{', '[' => try stack.append(c),
+            ')' => if (stack.items.len == 0 or stack.pop() != '(') return false,
+            '}' => if (stack.items.len == 0 or stack.pop() != '{') return false,
+            ']' => if (stack.items.len == 0 or stack.pop() != '[') return false,
+            else => continue,
+        }
+    }
+    return stack.items.len == 0;
 }
